1964a0470da343a06657d04ba7518bbadce04ca7
michaeldiogo253 <michaeldiogo17@outlook.com>
2020-05-19 13:40:55 -0300

    atualizaçoes

--- src/br/com/projeto/dao/FuncionariosDAO.java
+++ src/br/com/projeto/dao/FuncionariosDAO.java
@@ -1,10 +1,14 @@
 package br.com.projeto.dao;
 
 import br.com.projeto.jdbc.ConnectionFactory;
+import br.com.projeto.model.Clientes;
 import br.com.projeto.model.Funcionarios;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 /**
@@ -55,17 +59,145 @@ public class FuncionariosDAO {
         
     }
 
-    public void alterarFuncionario() {
+    public void alterarFuncionario(Funcionarios obj) {
 
+        try {
+            String SQL = "update tb_funcionarios set nome=?,rg=?,cpf=?,email=?,senha =?, cargo = ? , nivel_acesso = ?,telefone=?,celular=?,cep=?,endereco=?,numero=?,complemento=?,bairro=?,cidade=?,"
+                    + "estado=? where id=?"
+                    ;
+
+            PreparedStatement stmt = con.prepareStatement(SQL);
+            stmt.setString(1, obj.getNome());
+            stmt.setString(2, obj.getRg());
+            stmt.setString(3, obj.getCpf());
+            stmt.setString(4, obj.getEmail());
+            stmt.setString(5, obj.getSenha());
+            stmt.setString(6, obj.getCargo());
+            stmt.setString(7, obj.getNivel_Acesso());
+            stmt.setString(8, obj.getTelefone());
+            stmt.setString(9, obj.getCelular());
+            stmt.setString(10, obj.getCep());
+            stmt.setString(11, obj.getEndereco());
+            stmt.setInt(12, obj.getNumero());
+            stmt.setString(13, obj.getComplemento());
+            stmt.setString(14, obj.getBairro());
+            stmt.setString(15, obj.getCidade());
+            stmt.setString(16, obj.getUf());
+            stmt.setInt(17, obj.getId());
+
+            stmt.execute();
+            stmt.close();
+            JOptionPane.showMessageDialog(null, "Alterado com sucesso!");
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao alterar !" + e);
+
+        }
+
+        
     }
 
-    public void excluirFuncionario() {
+    public void excluirFuncionario(Funcionarios obj) {
+        try {
+            String SQL = "delete from tb_funcionarios where id = ?";
 
-    }
-    
-    
-    public void buscaFuncionarioPorNome(String nome){
+            PreparedStatement stmt = con.prepareStatement(SQL);
+            stmt.setInt(1, obj.getId());
+            
+
+            stmt.execute();
+            stmt.close();
+            JOptionPane.showMessageDialog(null, "Excluido  com sucesso!");
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao excluir !" + e);
+
+        }
+        
         
     }
     
-}
+
+        public List<Funcionarios> listarFuncionarios() {
+
+        try {
+            List<Funcionarios> lista = new ArrayList<>();  // criar a lista
+            String SQL = "select * from tb_funcionarios";
+            PreparedStatement stmt = con.prepareStatement(SQL);
+            ResultSet rs = stmt.executeQuery(SQL);
+
+            while (rs.next()) {
+                Funcionarios obj = new Funcionarios();
+                obj.setId(rs.getInt("id"));
+                obj.setNome(rs.getString("nome"));
+                obj.setRg(rs.getString("rg"));
+                obj.setCpf(rs.getString("cpf"));
+                obj.setEmail(rs.getString("email"));
+                obj.setSenha(rs.getString("senha"));
+                obj.setCargo(rs.getString("cargo"));
+                obj.setNivel_Acesso(rs.getString("nivel_acesso"));
+                obj.setTelefone(rs.getString("telefone"));
+                obj.setCelular(rs.getString("celular"));
+                obj.setCep(rs.getString("cep"));
+                obj.setEndereco(rs.getString("endereco"));
+                obj.setNumero(rs.getInt("numero"));
+                obj.setComplemento(rs.getString("complemento"));
+                obj.setBairro(rs.getString("bairro"));
+                obj.setCidade(rs.getString("cidade"));
+                obj.setUf(rs.getString("estado")); // pode ser uf 
+                lista.add(obj);
+            }
+
+            return lista;
+
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro" + erro);
+            return null;
+        }
+
+    }
+    
+    public List<Funcionarios> buscaFuncionarioPorNome(String nome){
+        
+        try {
+            List<Funcionarios> lista = new ArrayList<>();  // criar a lista
+            String SQL = "select * from tb_funcionarios where nome like '%"+ nome +"%'";                       
+            PreparedStatement stmt = con.prepareStatement(SQL);
+           // stmt.setString(1, nome);
+            ResultSet rs = stmt.executeQuery(SQL);
+              
+            
+            while (rs.next()) {
+                Funcionarios obj = new Funcionarios();
+                obj.setId(rs.getInt("id"));
+                obj.setNome(rs.getString("nome"));
+                obj.setRg(rs.getString("rg"));
+                obj.setCpf(rs.getString("cpf"));
+                obj.setEmail(rs.getString("email"));
+                obj.setSenha(rs.getString("senha"));
+                obj.setCargo(rs.getString("cargo"));
+                obj.setNivel_Acesso(rs.getString("nivel_acesso"));
+                obj.setTelefone(rs.getString("telefone"));
+                obj.setCelular(rs.getString("celular"));
+                obj.setCep(rs.getString("cep"));
+                obj.setEndereco(rs.getString("endereco"));
+                obj.setNumero(rs.getInt("numero"));
+                obj.setComplemento(rs.getString("complemento"));
+                obj.setBairro(rs.getString("bairro"));
+                obj.setCidade(rs.getString("cidade"));
+                obj.setUf(rs.getString("estado")); // pode ser uf 
+                lista.add(obj);
+            }
+
+            return lista;
+
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro" + erro);
+            return null;
+        }
+
+    }
+        
+    }
+    
+
